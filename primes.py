a0d6e7a0117de164450cee9992af98e394412c81
deathscytheh <alejandro.deathscythe@gmail.com>
2016-01-12 16:11:08 -0600

    Python 1 - Prime Class.
    Python Ej 1 - Modulo para calcular si un numero es primo.

--- primes.py
+++ primes.py
@@ -1,16 +1,33 @@
 """
-Your module documentation here
+A module to calculate if a number is prime.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    A class to calculate if a number is prime.
     """
 
     def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
+        """Returns True if num_int is prime"""
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int == 1:
+            return False
+        if num_int == 2:
+            return True
+        if num_int == 3:
+            return True
+        if num_int % 2 == 0:
+            return False
+        if num_int % 3 == 0:
+            return False
+
+        i_int = 5
+        w_int = 2
+        while i_int * i_int <= num_int:
+            if num_int % i_int == 0:
+                return False
+            i_int += w_int
+            w_int = 6 - w_int
+
+        return True
