a230dc3e36d602764e4e131b6f34d94d6f0b90ed
deathscytheh <alejandro.deathscythe@gmail.com>
2016-01-12 20:08:03 -0600

    SQL 1 - Ejercicio 1 de sql
    Se crearon tablas employee y employee_department, con sus respectivos
    campos y se population.

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -3,11 +3,33 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
+CREATE TABLE employee_department(
+  id int primary key,
+  name varchar(50),
+  description varchar(80)
 );
 
-CREATE TABLE employee_department (
+CREATE TABLE employee(
+  id int primary key,
+  id_dep int references employee_department(id),
+  first_name varchar(40),
+  last_name varchar(40)
 );
+
+-- Crear 4 empleados y 6 departamentos
+INSERT INTO employee_department values
+(1, 'HR', 'Recursos humanos'),
+(2, 'Ventas', 'Aqui vendemos'),
+(3, 'Compras', 'Aqui compramos'),
+(4, 'Sistemas', 'Aqui trabajamos'),
+(5, 'Direccion', 'Aqui solo cobramos'),
+(6, 'Contabilidad', 'Aqui lloras al ver tu cheque');
+
+INSERT INTO employee values
+(1, 1, 'hola', 'que hace'),
+(2, 2, 'ola', 'ke ase'),
+(3, 3, 'manuna', 'soy asesina'),
+(4, 4, 'pelon', 'enojon');
 
 CREATE TABLE employee_hobby (
 );
