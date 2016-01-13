3c9c71f236af260b07f6f3de4db615f888602699
deathscytheh <alejandro.deathscythe@gmail.com>
2016-01-12 21:19:33 -0600

    SQL 2 - Crear tabla hobby
    Se creo tabla hobby con sus datos. Y se le asigno hobbies a los
    empleados.

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -9,6 +9,14 @@ CREATE TABLE employee_department(
   description varchar(80)
 );
 
+-- Crear tabla hobby
+CREATE TABLE employee_hobby (
+  id int primary key,
+  name varchar(50),
+  description varchar(80)
+);
+
+-- Crear tabla empleado
 CREATE TABLE employee(
   id int primary key,
   id_dep int references employee_department(id),
@@ -16,7 +24,13 @@ CREATE TABLE employee(
   last_name varchar(40)
 );
 
--- Crear 4 empleados y 6 departamentos
+-- Crear tabla asignar hobbies a empleados
+CREATE TABLE employee_hobby_assign(
+  id_emp int references employee(id),
+  id_hobby int references employee_hobby(id)
+);
+
+-- Crear 6 departamentos
 INSERT INTO employee_department values
 (1, 'HR', 'Recursos humanos'),
 (2, 'Ventas', 'Aqui vendemos'),
@@ -25,13 +39,27 @@ INSERT INTO employee_department values
 (5, 'Direccion', 'Aqui solo cobramos'),
 (6, 'Contabilidad', 'Aqui lloras al ver tu cheque');
 
+-- Crear 3 hobbies
+INSERT INTO employee_hobby values
+(1, 'Futbol', '22 weyes detras de un balon'),
+(2, 'Basketball', '10 weyes detras de un balon'),
+(3, 'Golf', 'Un wey con lana detras de una pelota');
+
+-- Crear 4 empleados
 INSERT INTO employee values
 (1, 1, 'hola', 'que hace'),
 (2, 2, 'ola', 'ke ase'),
 (3, 3, 'manuna', 'soy asesina'),
 (4, 4, 'pelon', 'enojon');
 
-CREATE TABLE employee_hobby (
-);
-
+-- Assignar dos hobbies a empleados
+INSERT INTO employee_hobby_assign values
+(1, 1),
+(1, 2),
+(2, 3),
+(2, 1),
+(3, 2),
+(3, 3),
+(4, 1),
+(4, 2);
 -- ...
