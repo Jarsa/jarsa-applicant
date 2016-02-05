affe7d382676231dd19e6ce2222a5f141e1b4279
Daniel Rocha <danielrocha94@msn.com>
2016-02-05 12:33:18 -0600

    Python exercises solved

--- calculator.py
+++ calculator.py
@@ -1,16 +1,15 @@
-"""
-Your module documentation here
-"""
-
-
+'''
+The input must be a list of numbers,
+then the method sum returns the sum of the numbers in the list
+'''
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
+    '''
+    Sum method will return the sum of the number list as an integer
+    '''
+    @staticmethod
+    def sum(num_list):
+        return reduce(lambda n, m: n+m, num_list)
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+if __name__ == '__main__':
+    num_list = input("Escribe una lista de numberos: ")
+    print(CalculatorClass().sum(num_list))
--- primes.py
+++ primes.py
@@ -1,16 +1,31 @@
-"""
-Your module documentation here
-"""
+from math import sqrt; from itertools import count, islice
 
+'''
+    PrimeClass contains is_prime method that returns
+    true if the number input is a prime number, otherwise
+    returns false
+'''
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+    '''
+        the is_prime method receives a number integer
+        and enters a loop to calculate 'number % i',
+        where 'i' is all the numbers between 2 and
+        the square root of 'number', if the number is 1 or less
+        returns false
+    '''
+    @staticmethod
+    def is_prime(number):
+        if number > 1:
+            for i in islice(count(2), int(sqrt(number))):
+                if(number % i) == 1 or number == 2:
+                    return True
+                elif (number % i) == 0:
+                    return False
+        else:
+            return False
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+if __name__ == '__main__':
+    number = int(input("Escribe un numero entero: "))
+    print PrimeClass.is_prime(number)
+
