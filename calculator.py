fcdd785d2e3f66ff9376c31fb841c57598bd9aa3
Alejandra Muñoz Villalobos <alemv18@hotmail.com>
2016-02-05 23:50:28 -0600

    Cambios2

--- calculator.py
+++ calculator.py
@@ -7,6 +7,6 @@ class CalculatorClass(object):
         return reduce(lambda n, m: n+m, num_list)
 
 if __name__ == '__main__':
-    num_list = raw_input("Ingresa los numeros a sumar:")
+    num_list = input("Ingresa los numeros a sumar:")
     print((CalculatorClass().sum(num_list)))
 
