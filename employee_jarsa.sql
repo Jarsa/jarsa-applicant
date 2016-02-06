96214f0e21a6a27d1f7a0af116d03450d7d0aef1
Alejandra Muñoz Villalobos <alemv18@hotmail.com>
2016-02-05 23:55:06 -0600

    Cambios

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -1,28 +1,32 @@
-
 CREATE DATABASE employee_employee;
+USE employee_employee;
 
 CREATE TABLE employee(
-    employeeID int NOT NULL PRIMARY KEY,
+    employeeID int PRIMARY KEY NOT NULL,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL, 
-    departmentID int FOREIGN KEY REFERENCES employee_department(departmentID),
-    bossID varchar(40) FOREIGN KEY REFERENCES employee(bossID)
+    departmentID int,
+    FOREIGN KEY (departmentID) REFERENCES employee_department(departmentID),
+    bossID varchar(40),
+    FOREIGN KEY (bossID) REFERENCES employee(bossID)
 );
 
 CREATE TABLE employee_department(
     departmentID int PRIMARY KEY NOT NULL,  
     name varchar(30) NOT NULL, 
-    description(60) NOT NULL
+    description varchar(60) 
 );
 
 CREATE TABLE employee_hobby(
-  hobbyID int NOT NULL PRIMARY KEY,
+  hobbyID int PRIMARY KEY NOT NULL,
   name varchar(40) NOT NULL, 
   description varchar(60) NOT NULL
 );
 
  CREATE TABLE employeeHobby(
-  employeeID int REFERENCES employee(employeeID),
-  hobbyID int REFERENCES employee_hobby(hobbyID),
+  employeeID int,
+  FOREIGN KEY (employeeID) REFERENCES employee(employeeID),
+  hobbyID int,
+  FOREIGN KEY (hobbyID) REFERENCES employee_hobby(hobbyID),
   PRIMARY KEY (employeeID, hobbyID)
 );
