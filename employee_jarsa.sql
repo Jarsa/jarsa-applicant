59aaa5e5e522fca5e172271870dc972f5714316f
Alejandra Muñoz Villalobos <alemv18@hotmail.com>
2016-02-06 18:15:49 -0600

    Add foreign key from department_id

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE employee_employee;
-USE employee_employee;
+-- CREATE DATABASE employee_employee;
+-- USE employee_employee;
 
 CREATE TABLE employee(
     employeeID int PRIMARY KEY NOT NULL,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL, 
-    departmentID int,
-    FOREIGN KEY (departmentID) REFERENCES employee_department(departmentID),
-    bossID varchar(40),
-    FOREIGN KEY (bossID) REFERENCES employee(bossID)
+    -- departmentID int,
+    -- FOREIGN KEY (departmentID) REFERENCES employee_department(departmentID),
+    -- bossID varchar(40),
+    -- FOREIGN KEY (bossID) REFERENCES employee(bossID)
 );
 
 CREATE TABLE employee_department(
@@ -16,6 +16,9 @@ CREATE TABLE employee_department(
     name varchar(30) NOT NULL, 
     description varchar(60) 
 );
+
+ALTER TABLE employee
+ADD FOREIGN KEY (departmentID) REFERENCES employee_department(departmentID);
 
 CREATE TABLE employee_hobby(
   hobbyID int PRIMARY KEY NOT NULL,
