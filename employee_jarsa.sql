0980f5984a6bd7d608c134f501f82c3a71b28706
danielrocha94 <danielrocha94@msn.com>
2016-02-08 17:11:51 -0600

    Solving 2nd SQL Exercise

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -17,7 +17,8 @@ CREATE TABLE employee_department (
 );
 
 
-ALTER TABLE employee ADD FOREIGN KEY(department_id) REFERENCES employee_department(id)
+ALTER TABLE employee
+ADD FOREIGN KEY(department_id) REFERENCES employee_department(id)
 
 
 
@@ -43,6 +44,40 @@ INSERT INTO employee VALUES('103', 'Andres', 'Hernandez', '202')
 
 
 CREATE TABLE employee_hobby (
+id int NOT NULL PRIMARY KEY,
+name varchar(40),
+description text
 );
+
+CREATE TABLE employee_employee_hobby(
+employee_id int,
+hobby_id int
+);
+
+ALTER TABLE employee_employee_hobby
+ADD FOREIGN KEY(employee_id) REFERENCES employee(id);
+
+ALTER TABLE employee_employee_hobby
+ADD FOREIGN KEY(hobby_id) REFERENCES employee_hobby(id);
+
+INSERT INTO employee_hobby VALUES('3', 'Socializing', 'The employee enjoys spending time with his/her friends whenever possible?');
+
+INSERT INTO employee_hobby VALUES('2', 'Video Games', 'The employee frecuently play video games?');
+
+INSERT INTO employee_hobby VALUES ('1', 'Exercise', 'The employee likes running or goes to the gym on his/her free time?');
+
+INSERT INTO employee_employee_hobby VALUES(101,3);
+
+INSERT INTO employee_employee_hobby VALUES(101,2);
+
+INSERT INTO employee_employee_hobby VALUES(102,1);
+
+INSERT INTO employee_employee_hobby VALUES(102,3);
+
+INSERT INTO employee_employee_hobby VALUES(103,1);
+
+INSERT INTO employee_employee_hobby VALUES(103,2);
+
+INSERT INTO employee_employee_hobby VALUES(103,3);
 
 -- ...
