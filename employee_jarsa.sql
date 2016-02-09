227f6ab3dd71620dc425c012c214200f58123ddf
alemv18 <alemv18@hotmail.com>
2016-02-09 15:49:58 -0600

    Employee and department tables

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -1,12 +1,7 @@
--- Your sql code in this file
--- NOTE: Please, don't add sentence to create database in this script file.
---       You can create database locally to test it.
---       Consider add ';' at end sentence.
-
 CREATE TABLE employee (
 employee_id int PRIMARY KEY NOT NULL,
 first_name varchar(40) NOT NULL, 
-last_name varchar(50) NOT NULL
+last_name varchar(50) NOT NULL,
 department_id int
 );
 
@@ -19,20 +14,16 @@ description varchar(50) NOT NULL
 ALTER TABLE employee
 ADD FOREIGN KEY (department_id) REFERENCES employee_department(department_id);
 
-INSERT INTO employee VALUES ('100','Alejandra','Munoz','200');
-INSERT INTO employee VALUES ('101','Daniel','Rocha','201');
-INSERT INTO employee VALUES ('102','Benjamin','Arredondo','202');
-INSERT INTO employee VALUES ('103','Liliana','Barraza','203');
-
 INSERT INTO employee_department VALUES ('200','Maquiladora','Confeccion de ropa');
 INSERT INTO employee_department VALUES ('201','Business','Negocios internacionales');
 INSERT INTO employee_department VALUES ('202','IT Business','Administracion de tecnologias de la informacion');
 INSERT INTO employee_department VALUES ('203','Marketing','Publicidad');
 INSERT INTO employee_department VALUES ('204','Contabilidad','Negocios');
 INSERT INTO employee_department VALUES ('205','Diseno grafico','Publicidad y muchas cosas mas');
+INSERT INTO employee VALUES ('100','Alejandra','Munoz','200');
+INSERT INTO employee VALUES ('101','Daniel','Rocha','201');
+INSERT INTO employee VALUES ('102','Benjamin','Arredondo','202');
+INSERT INTO employee VALUES ('103','Liliana','Barraza','203');
 
 
-CREATE TABLE employee_hobby (
-);
 
--- ...
