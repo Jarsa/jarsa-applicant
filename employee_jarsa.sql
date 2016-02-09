8b143b82ab6b1a5a25308a311bda8f8ad782f7ab
alemv18 <alemv18@hotmail.com>
2016-02-09 17:51:57 -0600

    Employee and Hobby tables

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -25,5 +25,34 @@ INSERT INTO employee VALUES ('101','Daniel','Rocha','201');
 INSERT INTO employee VALUES ('102','Benjamin','Arredondo','202');
 INSERT INTO employee VALUES ('103','Liliana','Barraza','203');
 
+CREATE TABLE employee_hobby(
+id int PRIMARY KEY NOT NULL, 
+name varchar(50) NOT NULL, 
+description text
+);
 
+CREATE TABLE employee_employeehobby(
+employee_id int,
+id int
+);
+
+ALTER TABLE employee_employeehobby
+ADD FOREIGN KEY (employee_id) REFERENCES employee(employee_id);
+
+ALTER TABLE employee_employeehobby
+ADD FOREIGN KEY (id) REFERENCES employee_hobby(id);
+
+INSERT INTO employee_hobby VALUES ('10','Natacion','Swim and swim');
+INSERT INTO employee_hobby VALUES ('20','Jockey','Canada favorite sport');
+INSERT INTO employee_hobby VALUES ('30','Horse Riding','Be on horse riding');
+
+INSERT INTO employee_employeehobby VALUES ('100','10');
+INSERT INTO employee_employeehobby VALUES ('100','20');
+INSERT INTO employee_employeehobby VALUES ('100','30');
+INSERT INTO employee_employeehobby VALUES ('101','10');
+INSERT INTO employee_employeehobby VALUES ('101','20');
+INSERT INTO employee_employeehobby VALUES ('101','30');
+INSERT INTO employee_employeehobby VALUES ('102','30');
+INSERT INTO employee_employeehobby VALUES ('102','20');
+INSERT INTO employee_employeehobby VALUES ('102','10');
 
