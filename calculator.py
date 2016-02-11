62cab722682c8e62b216293cf99f9437d622f121
alemv18 <alemv18@hotmail.com>
2016-02-10 19:59:28 -0600

    Changes to calculator.py

--- calculator.py
+++ calculator.py
@@ -4,13 +4,17 @@ Develop a calculator that receives a list of numbers as an input and adds those 
 
 
 class CalculatorClass(object):
-    """ 
-    Select a type of method to be implemented.
     """
+        Select a type of method to be implemented.
+        """
     @staticmethod
-    def sum(num_list):
-        return reduce(lambda n, m: n+m, num_list)
+    def sum(numbers):
+        """
+           Adding numbers by reducing the list.
+            """
+        return reduce(lambda x, y: x+y, numbers)
 
-if __name__ == '__main__':
-    num_list = int(input("Write a list of numbers:"))
-    print(CalculatorClass().sum(num_list))
+#if __name__ == '__main__':
+#    numbers = int(input("Write a list of numbers:"))
+#    print CalculatorClass().sum(numbers)
+
