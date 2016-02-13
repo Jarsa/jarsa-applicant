b440ba58389bdcd59227555b1d8516316ce72792
alemv18 <alemv18@hotmail.com>
2016-02-13 13:20:12 -0600

    Last Changes2

--- primes.py
+++ primes.py
@@ -2,18 +2,21 @@
     Prime number input
     An integer is introduced as an input and the
     program calculates if it is a prime number or not.
-    """
+"""
 from math import sqrt
-from itertools import islice,count
+from itertools import islice, count
 
 
 class PrimeClass(object):
     """
         Define the type of method and the method
         itself that will be implemented.
-        """
+    """
     @staticmethod
-        def is_prime(num_int):
+    def is_prime(num_int):
+        '''
+           Try to find if the number is prime or not.
+        '''
         if num_int > 1:
             for i in islice(count(2), int(sqrt(num_int))):
                 if num_int == 2:
@@ -23,8 +26,3 @@ class PrimeClass(object):
             return True
         else:
             return False
-
-
-if __name__ =='__main__':
-    alejandra = int(input('escribe algo:'))
-    print (PrimeClass.is_prime(alejandra))
