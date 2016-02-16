b54d1f2f6154e4a88c3ecbdc4f524d2acd827977
alemv18 <alemv18@hotmail.com>
2016-02-15 19:26:29 -0600

    Change to index

--- primes.py
+++ primes.py
@@ -18,10 +18,10 @@ class PrimeClass(object):
            Try to find if the number is prime or not.
         '''
         if num_int > 1:
-            for _ in islice(count(2), int(sqrt(num_int))):
+            for index in islice(count(2), int(sqrt(num_int))):
                 if num_int == 2:
                     return True
-                elif (num_int % _) == 0:
+                elif (num_int % index) == 0:
                     return False
             return True
         else:
