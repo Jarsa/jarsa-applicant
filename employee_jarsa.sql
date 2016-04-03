712cd50ffb510b6ff621587a0aee586cc55c9183
Lázaro Rodríguez <lazaro@outlook.com>
2016-04-02 22:14:10 -0600

    SQL2

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -18,6 +18,17 @@ CREATE TABLE employee_department (
 );
 
 CREATE TABLE employee_hobby (
+    id serial PRIMARY KEY,
+    name varchar(30),
+    description varchar(200)
+);
+
+
+CREATE TABLE employee_hobby_rel (
+    employee_id integer,
+    hobby_id integer,
+    FOREIGN KEY(employee_id) REFERENCES employee(id),
+    FOREIGN KEY(hobby_id) REFERENCES employee_hobby(id)--// In both cases the primary key is ID field
 );
 
 INSERT INTO employee VALUES (1, 'Paola', 'García Cabrales', 6);
@@ -31,5 +42,18 @@ INSERT INTO employee_department VALUES (3, 'Recursos Humanos','El departamento d
 INSERT INTO employee_department VALUES (4, 'Manufactura', 'El departamento de manufactura se encarga de...');
 INSERT INTO employee_department VALUES (5, 'IDS', 'El departamento de investigación y desarrollo de software se encarga de ...');
 INSERT INTO employee_department VALUES (6, 'Ventas', 'El departamento de ventas se encarga de ...');
+
+INSERT INTO employee_hobby VALUES (1, 'Jugar ajedrez', 'Jugar ajedrez en lichess.org');
+INSERT INTO employee_hobby VALUES (2, 'Ver anime', 'Ver anime en JK Anime');
+INSERT INTO employee_hobby VALUES (3, 'Jugar basketball', 'Jugar basketball en la canchitas');
+
+INSERT INTO employee_hobby_rel VALUES (1, 1);
+INSERT INTO employee_hobby_rel VALUES (1, 2);
+INSERT INTO employee_hobby_rel VALUES (2, 1);
+INSERT INTO employee_hobby_rel VALUES (2, 2);
+INSERT INTO employee_hobby_rel VALUES (3, 2);
+INSERT INTO employee_hobby_rel VALUES (3, 3);
+INSERT INTO employee_hobby_rel VALUES (4, 2);
+INSERT INTO employee_hobby_rel VALUES (4, 3);
 
 -- ...
