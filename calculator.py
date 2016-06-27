a4479f6cfbfa90492b80f75f8ebe45215d502bdd
ougc27 <oscar_garzacordova@hotmail.com>
2016-06-27 13:57:39 -0500

    Modificación del archivo calculator.py para la implementación de un método que calcule la suma de una lista de números y retorne su resultado

--- calculator.py
+++ calculator.py
@@ -1,16 +1,18 @@
 """
-Your module documentation here
+This module contains one class named CalculatorClass. The class contains one method for calculate the sum of a list of numbers
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    This class has one method named sum, this method can return one variable named result that will contain the sum of the list of numbers given as parameter in the method.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        This method receive one parameter named num_list, that parameter will receive a list of numbers. Inside the method we use the sum function and we pass the input parameter, this function returns the sum of a list of numbers given as parameter, we save this sum in one variable named result and finally we return that variable that contains the result of the sum.
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        result = sum(num_list)
+        return result
+            
