2cf1ce64bfe56840f3904b74041ed50d4b90153b
IsabelEsparza <isa_tkm_ris@live.com.mx>
2016-09-16 20:20:49 -0500

    point 3 sql

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -29,3 +29,38 @@ insert into employee VALUES (2, 'Abraham', 'Avila Esparza',2);
 insert into employee VALUES (3, 'Abigail', 'Avila Esparza',3);
 insert into employee VALUES (4, 'Ana', 'Esparza Arg',3);
 
+CREATE TABLE employee_hobby (
+	id int primary key,
+	name varchar(40) not null,
+	description varchar(30) not null
+);
+
+CREATE TABLE employees_h(
+ id_employee int NOT NULL, 
+ id_employee_h INT
+);
+
+ALTER TABLE employees_h ADD CONSTRAINT fk_employee_employee_h FOREIGN KEY(id_employee) REFERENCES employee(id);
+ALTER TABLE employees_h ADD CONSTRAINT fk_employeehobby_employeesh FOREIGN KEY(id_employee_h) REFERENCES employee_hobby(id);
+
+
+insert INTO employee_hobby  VALUES (1, 'TKD','PRACTICE');
+insert INTO employee_hobby  VALUES (2, 'DANCE','PRACTICE');
+insert INTO employee_hobby  VALUES (3, 'PLAY GUITAR','PRACTICE');
+
+
+insert INTO employees_h  VALUES (1,2);
+insert INTO employees_h  VALUES (1,3);
+insert INTO employees_h  VALUES (2,2);
+insert INTO employees_h  VALUES (2,3);
+insert INTO employees_h  VALUES (3,1);
+insert INTO employees_h  VALUES (3,3);
+insert INTO employees_h  VALUES (4,2);
+insert INTO employees_h  VALUES (4,1);
+
+
+ALTER TABLE employee ADD COLUMN idboss INT DEFAULT NULL;
+UPDATE employee SET idboss = 1 WHERE id = 1;
+UPDATE employee SET idboss = 1 WHERE id = 2;
+UPDATE employee SET idboss = 1 WHERE id = 3;
+UPDATE employee SET idboss = 1 WHERE id = 4;
