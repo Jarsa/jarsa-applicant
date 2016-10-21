5db1b0832edcaea362af67cbb53b89bb1d77b150
Manuel Márquez <buzondemam@gmail.com>
2016-10-21 10:24:11 -0400

    New sql added for create tables employee and departments.

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -4,12 +4,33 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+    id INT PRIMARY KEY,   
+    first_name varchar(64),
+    last_name varchar(64)
 );
 
 CREATE TABLE employee_department (
+    id INT PRIMARY KEY,
+    name varchar(64),
+    description varchar(64)
 );
 
 CREATE TABLE employee_hobby (
 );
 
+ALTER TABLE employee ADD department_id INT REFERENCES employee_department(id);
+
+INSERT INTO employee_department (id, name, description) VALUES
+    (1, 'Sales', 'Sales Department'),
+    (2, 'Purchasing', 'Purchasing Department'),
+    (3, 'Marketing', 'Marketing Department'),
+    (4, 'HR', 'HR Department'),
+    (5, 'Technology', 'Technology Department'),
+    (6, 'Accounting', 'Accounting Department');
+
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES
+    (1, 'Manuel', 'Marquez', 5),
+    (2, 'Pedro', 'Perez', 6),
+    (3, 'Mike', 'Smith', 2),
+    (4, 'John',  'Doe', 1);
 -- ...
