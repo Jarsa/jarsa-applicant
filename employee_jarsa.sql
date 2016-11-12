9a653465346a3e0c74d630f892bcbd48be04cdd9
root <root@localhost.localdomain>
2016-11-12 01:35:31 -0600

    task 1 completed

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -3,13 +3,28 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
+--employee_department
+CREATE TABLE employee_department (dep_id serial primary key, name varchar(60), description varchar(250)
 );
+INSERT INTO employee_department (name, description) values
+	('Sistemas', 'Area de programadores e innovaciones tecnologicas'),
+	('Administracion', 'Area de papeleo y documentacion de la empresa'),
+	('Recursos Humanos', 'Area de reclutaciones y contrataciones'),
+	('Finanzas', 'Area de movimiento de dinero'),
+	('Recursos', 'Area de control de instalaciones, etc'),
+	('Produccion', 'Area de empleados en produccion');
 
-CREATE TABLE employee_department (
+--employee
+CREATE TABLE employee (id serial primary key, first_name varchar(50), last_name varchar(50),
+	dep_id int references employee_department(dep_id)
 );
+INSERT INTO employee (first_name, last_name, dep_id) values
+	('Pedro', 'Ramirez', 1),('Mario', 'Perez', 2),('Jorge', 'Estrada', 2),('Alberto', 'Guzman', 3);
 
-CREATE TABLE employee_hobby (
-);
+/*CREATE TABLE employee_hobby (
+);*/
+
+SELECT * from employee_department;
+SELECT * from employee;
 
 -- ...
