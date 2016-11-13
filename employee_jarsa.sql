7fa9fb02fce8e79b16a0034715cde40af3a4c238
AlexisReza <alexis.reza.s@gmail.com>
2016-11-13 17:30:06 -0600

    task 2 comleted

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -4,7 +4,7 @@
 --       Consider add ';' at end sentence.
 
 --employee_department
-CREATE TABLE employee_department (dep_id serial primary key, name varchar(60), description varchar(250)
+CREATE TABLE employee_department (id serial primary key, name varchar(60), description varchar(250)
 );
 INSERT INTO employee_department (name, description) values
 	('Sistemas', 'Area de programadores e innovaciones tecnologicas'),
@@ -16,15 +16,26 @@ INSERT INTO employee_department (name, description) values
 
 --employee
 CREATE TABLE employee (id serial primary key, first_name varchar(50), last_name varchar(50),
-	dep_id int references employee_department(dep_id)
+	dep_id int references employee_department(id)
 );
 INSERT INTO employee (first_name, last_name, dep_id) values
 	('Pedro', 'Ramirez', 1),('Mario', 'Perez', 2),('Jorge', 'Estrada', 2),('Alberto', 'Guzman', 3);
 
-/*CREATE TABLE employee_hobby (
-);*/
+--hobbies
+CREATE TABLE employee_hobby (id serial primary key, name varchar(60), description varchar(250)
+);
+INSERT INTO employee_hobby (name, description) values
+	('Ajedrez', 'Juego de mesa'),('Futbol', 'Deporte tradicional'),('Arte', 'Cualquiera tipo de arte');
+
+--employee_has_hobbies
+CREATE TABLE employee_has_hobbies (emp_id int references employee(id), hob_id int references employee_hobby(id)
+);
+INSERT INTO employee_has_hobbies (emp_id, hob_id) values
+	(1,1),(1,2),(2,1),(2,3),(3,2),(3,3),(4,1),(4,3);
 
 SELECT * from employee_department;
 SELECT * from employee;
+SELECT * from employee_hobby;
+SELECT * from employee_has_hobbies;
 
 -- ...
