df3607a907c430db9bf452bf6c819c7588d4e556
AlexisReza <alexis.reza.s@gmail.com>
2016-11-13 17:59:24 -0600

    task 2 comleted (errors fixed)

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -3,6 +3,12 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+--employee
+CREATE TABLE employee (id serial primary key, first_name varchar(50), last_name varchar(50)
+);
+INSERT INTO employee (first_name, last_name) values
+	('Pedro', 'Ramirez'),('Mario', 'Perez'),('Jorge', 'Estrada'),('Alberto', 'Guzman');
+
 --employee_department
 CREATE TABLE employee_department (id serial primary key, name varchar(60), description varchar(250)
 );
@@ -14,12 +20,12 @@ INSERT INTO employee_department (name, description) values
 	('Recursos', 'Area de control de instalaciones, etc'),
 	('Produccion', 'Area de empleados en produccion');
 
---employee
-CREATE TABLE employee (id serial primary key, first_name varchar(50), last_name varchar(50),
+--employee_has_department
+CREATE TABLE employee_has_department (emp_id int unique references employee(id),
 	dep_id int references employee_department(id)
 );
-INSERT INTO employee (first_name, last_name, dep_id) values
-	('Pedro', 'Ramirez', 1),('Mario', 'Perez', 2),('Jorge', 'Estrada', 2),('Alberto', 'Guzman', 3);
+INSERT INTO employee_has_department (emp_id, dep_id) values
+	(1,1),(2,2),(3,2),(4,3);
 
 --hobbies
 CREATE TABLE employee_hobby (id serial primary key, name varchar(60), description varchar(250)
@@ -33,8 +39,9 @@ CREATE TABLE employee_has_hobbies (emp_id int references employee(id), hob_id in
 INSERT INTO employee_has_hobbies (emp_id, hob_id) values
 	(1,1),(1,2),(2,1),(2,3),(3,2),(3,3),(4,1),(4,3);
 
-SELECT * from employee_department;
 SELECT * from employee;
+SELECT * from employee_department;
+SELECT * from employee_has_department;
 SELECT * from employee_hobby;
 SELECT * from employee_has_hobbies;
 
