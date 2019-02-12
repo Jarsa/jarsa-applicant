df33da450dbe7831974dd6951a1fba2f3b0c986d
jesus01x <xzan12@outlook.com>
2019-02-12 14:41:39 -0600

    jesus alcala python 2

--- calculator.py
+++ calculator.py
@@ -4,13 +4,31 @@ Your module documentation here
 
 
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
+	"""
+	Your class documentation here
+	"""
+	def __init__(self):
+		print("init")
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+	def readnumber(self):
+		listnumber1 = []
+		while True:
+			numero = input("inserta un numero (0) salir >> ")
+			val = int(numero)
+			if(val==0):
+				if(len(listnumber1)>0):
+					self.sum(listnumber1)
+				else:
+					print("there is not any number added to list")
+			else:
+				listnumber1.append(val)
+
+	def sum(self, num_list):
+		suma = 0
+		for num in num_list:
+			suma = suma + num
+		return print("The mount is equals to :"+str(suma))
+
+calculator = CalculatorClass()
+
+calculator.readnumber()
