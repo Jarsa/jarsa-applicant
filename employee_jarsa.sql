4b8a245453818bc8b7647b6a80d90b7226b9cc80
Juventino Villegas <2tino480@gmail.com>
2019-03-31 14:06:13 -0600

    psql3

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -2,25 +2,12 @@
 -- NOTE: Please, don't add sentence to create database in this script file.
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
+CREATE TABLE boss(id serial PRIMARY KEY, employee_id int 
+REFERENCES employee(id);
 
-CREATE TABLE employee_hobby(id serial PRIMARY KEY, name VARCHAR (50), 
-description VARCHAR (50));
+INSERT INTO boss (employee_id) VALUES ('1'),('3');
 
+ALTER TABLE employee ADD COLUMN boss_id int REFERENCES boss (id);
 
-INSERT INTO employee_hobby (name, description) VALUES 
-('basketball','shoot balls into a basket'),
-('chess','really old game'),
-('woodworking','building furniture');
-
-CREATE TABLE employee_hobbies(employee_id int 
-REFERENCES employee (id) ON UPDATE CASCADE ON 
-DELETE CASCADE, hobby_id int REFERENCES 
-employee_hobby (id) ON UPDATE CASCADE, 
-CONSTRAINT employee_hobby_id_pkey PRIMARY KEY 
-(employee_id, hobby_id));
-
-INSERT INTO employee_hobbies (employee_id, hobby_id) 
-VALUES ('1','1'),('1','2'),('2','2'),('2','3'),('3','2'),
-('3','3'),('4','1'),('4','3');
 
 -- ...
