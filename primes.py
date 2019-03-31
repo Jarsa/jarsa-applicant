7abc678bda2f99fc57747c944df6febd102025d2
Juventino Villegas <2tino480@gmail.com>
2019-03-31 11:15:32 -0600

    Finished primes

--- primes.py
+++ primes.py
@@ -5,12 +5,35 @@ Your module documentation here
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    This class takes a number from the user
+    parses it into an integer then runs a 
+    function to determine if the number is prime
+    if the number in questrion is prime it 
+    returns true else it returns false
+    
     """
+    num = input('Input your number ')
+    num = int(num)
+    
 
-    def is_prime(self, num_int):
+
+    def is_prime(n):
         """
-        Your method documentation here
+        This function takes a parameter n 
+        it checks this parameter against the first two numbers 
+        1 and 2 if its neither 1 or two it starts a loop
+        where it checks if any number inbetween 2 and n
+        is a multiple of n 
+
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if (n==1):
+            return False
+        elif (n==2):
+            return True;
+        else:
+            for x in range(2,n):
+                if(n % x==0):
+                    return False
+        return True
+    print (is_prime(num))
