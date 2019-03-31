15b9a86c1fd3d1156122f46dbc71c00a21fa1c1e
Juventino Villegas <2tino480@gmail.com>
2019-03-31 13:27:39 -0600

    calculator

--- calculator.py
+++ calculator.py
@@ -5,12 +5,25 @@ Your module documentation here
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    This class takes aq number from the user
+    then it uses numbers.split to split the string
+    by spaces and add it to a list after that we
+    have a function that takes as a parameter the
+    list we generated and outputs the sum of the list
     """
+    numbers = raw_input("Enter your numbers seperated by a space ")
+    list = numbers.split()
 
-    def sum(self, num_list):
+    def sum(num_list):
         """
-        Your method documentation here
+        This function takes a list as a parameter
+        it parses each number in the list and
+        adds it to the variable sum after which it
+        returns sum
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        sum = 0
+        for num in num_list:
+            sum += int(num)
+        return(sum)
+    print(sum(list))
