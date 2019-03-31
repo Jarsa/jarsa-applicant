43a43a9d1bcb650a1310505330629e283ba2010c
Juventino Villegas <2tino480@gmail.com>
2019-03-31 11:09:10 -0600

    Finished first problem

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -3,13 +3,29 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
-);
-
 CREATE TABLE employee_department (
+id serial PRIMARY KEY, name VARCHAR (50), 
+description VARCHAR(50)
 );
 
-CREATE TABLE employee_hobby (
+CREATE TABLE employee (
+id serial PRIMARY KEY, first_name VARCHAR (50),
+last_name VARCHAR (50), dep_id serial,
+FOREIGN KEY (dep_id) REFERENCES employee_department
+(id)
 );
+
+INSERT INTO employee_department (name, description) 
+VALUES ('accounting', 'takes care of all company accounts'),
+('hr','human resources'),('manufacturing','builds the product'),
+('sales','sells the product'),('management','manages activities'),
+('programers','build the software');
+
+INSERT INTO employee (first_name, last_name, dep_id) 
+VALUES ('John', 'Doe', '2'),
+('Jane','Doe', '2'),('Fulano','Mangado', '1'),
+('Fulana','Mangada', '5');
+
+
 
 -- ...
