834e54d5424c58cd24e5c9a6e78cf9456fddc51d
Juventino Villegas <2tino480@gmail.com>
2019-04-01 02:15:12 -0600

    psql 2nd try first file

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -2,12 +2,34 @@
 -- NOTE: Please, don't add sentence to create database in this script file.
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
-CREATE TABLE boss(id serial PRIMARY KEY, employee_id int 
-REFERENCES employee(id);
+CREATE TABLE employee(
+id serial PRIMARY KEY,
+first_name varchar(50) NOT NULL, 
+last_name varchar(50) NOT NULL,
+dep_id int
+);
 
-INSERT INTO boss (employee_id) VALUES ('1'),('3');
+CREATE TABLE employee_department (
+id serial PRIMARY KEY, 
+name varchar(50) NOT NULL, 
+description varchar(50) NOT NULL 
+);
 
-ALTER TABLE employee ADD COLUMN boss_id int REFERENCES boss (id);
 
+ALTER TABLE employee
+ADD FOREIGN KEY (department_id) REFERENCES employee_department(id);
 
+INSERT INTO employee_department (name, description)
+VALUES ('Accounting','Handles the money'),
+('HR', 'Human Resources'),
+('Management', 'Manages everyone'),
+('Manufacturing', 'Produces the product'),
+('RD', 'Research and Development'),
+('Tech', 'Technical Department');
+
+INSERT INTO employee (first_name, last_name, dep_id)
+VALUES ('John','Doe','2'),
+('Jane', 'Doe', '3'),
+('Fulano', 'Mangano','2'),
+('Fulana', 'Mangana', '1');
 -- ...
