a6dc22df972fc09ee455262f9f3db884e0e0ab6a
edgar.toral.torres <edgar_toral@outlook.com>
2019-08-10 15:45:17 -0500

    PYTHON1

--- primes.py
+++ primes.py
@@ -1,16 +1,24 @@
 """
-Your module documentation here
+Here is the class with its method or function to evaluation.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Inside is the function to determinate if a number is prime or not.
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Negative numbers are not primes.
+        A prime number is a number that is only divisible by itself and one.
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int < 1:
+            return False
+        elif num_int == 2:
+            return True
+        else:
+            for i in range(2, num_int):
+                if num_int % i == 0:
+                    return False
+                return True
