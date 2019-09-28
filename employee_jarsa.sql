7503de19d01daa0b541732699cbfc688ffa8167d
Francisco Ordaz IV <fcescoiv@gmail.com>
2019-09-27 22:19:21 -0500

    SQL2: Added/edited table(s) to enable hobby registry for employees

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -7,43 +7,52 @@ CREATE TABLE employee (
     id serial PRIMARY KEY,
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
-    employee_department INTEGER REFERENCES employee_department (department_id)
+    employee_department_id INTEGER REFERENCES employee_department (id)
+    --employee_hobby_id INTEGER REFERENCES employee_hobby (id) in case we want single hobby constraint
+    employee_hobbies_id INTEGER [] --primary key constraint not supported for integers afaik
 );
 
 CREATE TABLE employee_department (
-    department_id serial PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description VARCHAR(200) NOT NULL
+    id serial PRIMARY KEY,
+    deparment_name VARCHAR(100) NOT NULL,
+    department_description VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE employee_hobby (
+    id serial PRIMARY KEY
+    empHobby_name VARCHAR(50) NOT NULL,
+    empHobby_description VARCHAR (200) NOT NULL 
 );
 
 -- ...
 
-INSERT INTO employee (first_name, last_name, employee_department)
+INSERT INTO employee (first_name, last_name, employee_department_id, employee_hobbies_id )
 VALUES(
     'Mr meow',
     'whiskers',
-    1
+    1,
+    '{1,2}'
 ),
 (
     'Terry',
     'Crews',
-    2
+    2,
+    '{1,2,3}'
 ),
 (
     'John',
     'Smith',
-    2
+    2,
+    '{2,3}'
 ),
 (
     'Oliver',
     'Khan',
-    5
+    5,
+    '{1,2,3}'
 );
 
-INSERT INTO employee_department(name, description)
+INSERT INTO employee_department(department_name, department_description)
 VALUES(
     'Department of Justice',
     'Justiciary branch of the federal government tasked with uplholding the land'
@@ -68,3 +77,17 @@ VALUES(
     'IT',
     'Department in charge of all information technologies and their implementations'
 );
+
+INSERT INTO employee_hobby(empHobby_name, empHobby_description)
+VALUES(
+    'Napping',
+    'Likes to take naps.'
+),
+(
+    'Soccer',
+    'Sport played mostly with your feet.'
+),
+(
+    'Basketball',
+    'Sport where you gotta jump a lot.'
+)
