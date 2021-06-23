3cb5b9e5c6f6ebdb037b4b3c3e3782c6a7eef473
JazielRodH <jazielrodriguez_h@outlook.com>
2021-06-22 21:24:00 -0500

    Python1

--- primes.py
+++ primes.py
@@ -1,16 +1,32 @@
 """
-Your module documentation here
+Este modulo sirve para definir la clase así como el método que 
+utilizaremos para la saber si un numero es primo o no lo es.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Clase la cual contiene el método para saber si un número
+    es primo o no es primo
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Método el cual recibe el parametro para hacer las operaciones 
+        necesarias como el rango para conocer la naturaleza del número
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int < 2:
+            return print(False)
+        for i in range(2, num_int):
+            if num_int % i == 0:
+                return print(False)
+        return print(True)
+
+
+print("Ingresa el numero")
+num = int(input())
+
+
+a = PrimeClass()
+
+a.is_prime(num)
