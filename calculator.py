bafb132abf0299a7be3dd9ef5f856d5553ea6c35
JazielRodH <jazielrodriguez_h@outlook.com>
2021-06-22 22:04:16 -0500

    Python2

--- calculator.py
+++ calculator.py
@@ -1,16 +1,35 @@
 """
-Your module documentation here
+Modulo con las clases y métodos necesarios para realizar
+la suma de los items de una lista asignados por el usuario
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    Clase en la cual se crea el método necesario para realizar 
+    las suma de los numeros contenidos en la lista
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Este Metodo realiza la suma de los items de la lista los
+        cuales son recorridos por el ciclo for una vez termina el
+        ciclo retorna la suma
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        laSuma = 0
+        for i in num_list:
+            laSuma = laSuma + i
+        return print(laSuma)
+
+
+try:
+    liastai = []
+    n = int(input("¿Cuantos numeros desea sumar? "))
+    for i in range(n):
+        item = int(input("Numero " + str(i+1) + ": "))
+        liastai.append(item)
+
+    Lista = CalculatorClass()
+    Lista.sum(liastai)
+except:
+    print("Ingrese un numero valido")
