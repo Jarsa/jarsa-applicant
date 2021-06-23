4fb08ff091cc8ede9f3172a03eacc07a3f1152a0
JazielRodH <jazielrodriguez_h@outlook.com>
2021-06-22 19:11:34 -0500

    SQL1

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -3,13 +3,30 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+CREATE TABLE employee_department(
+	id serial not null primary key,
+	name varchar(50) not null,
+	description text not null
+);
+
+
 CREATE TABLE employee (
+	id serial not null primary key,
+	first_name varchar(25),
+	last_name varchar(25),
+	department int not null,
+	foreign key(department) references employee_department(id)
 );
 
-CREATE TABLE employee_department (
-);
 
-CREATE TABLE employee_hobby (
-);
+INSERT INTO employee_department(name, description) VALUES ('General Managment','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi'),
+														  ('Marketing','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi'),
+														  ('Operations','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi'),
+														  ('Finance','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi'),
+														  ('Sales','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi'),
+														  ('Purchase','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto non quasi');
 
--- ...
+INSERT INTO employee(first_name, last_name, department) VALUES ('Jaziel Abisai','Rodriguez Hernandez', 1),
+															   ('Joshua Isaí','Salas Saavedra',2),
+															   ('Ana Lizeth','Ramirez Rivas',4),
+															   ('Blanca Yessenia','Gaeta Talamantes',6);
